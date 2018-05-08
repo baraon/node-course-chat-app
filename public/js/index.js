09b7633db7a324106156fd964487c465c610c849
baraon <gallegosbaraon@yahoo.com>
2018-05-08 10:09:56 -0500

    Added CSS to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,28 +27,31 @@ socket.on('newLocationMessage', function(location) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
-
+  var messageTextbox = '[name=message]';
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: jQuery(messageTextbox).val()
   }, function () {
-
+    jQuery(messageTextbox).val('');
   });
 });
 
 var locationButton = jQuery('#send-location');
-
 locationButton.on('click', function() {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('unable to fetch location');
   });
 });

f3576d6f4241813fb89c66734ad928373b66995e
baraon <gallegosbaraon@yahoo.com>
2018-05-08 10:54:01 -0500

    added templates using mustache to front end

--- public/js/index.js
+++ public/js/index.js
@@ -9,22 +9,43 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  /*
   console.log('New Message: ', message);
   var li = jQuery('<li></li>');
   li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(li);*/
 
 });
 
 socket.on('newLocationMessage', function(location) {
+
+  var formattedTime = moment(location.createdAt).format('h:mm a');
+  var template = jQuery('#location-template').html();
+  var html = Mustache.render(template, {
+    text: location.url,
+    from: location.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+
+  /*
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>')
   var formattedTime = moment(location.createdAt).format('h:mm a');
   li.text(`${location.from} ${formattedTime}: `);
   a.attr('href', location.url);
   li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(li);*/
 });
 
 jQuery('#message-form').on('submit', function (e) {
